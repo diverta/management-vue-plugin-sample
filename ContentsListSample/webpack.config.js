88e3ad85b81f0f20f76702a07aa8fa22ee4b5355
sakaguchi-diverta <sakaguchi@diverta.co.jp>
2022-06-10 18:36:24 +0900

    fix: webpack configuration

--- ContentsListSample/webpack.config.js
+++ ContentsListSample/webpack.config.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { DefinePlugin } = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
@@ -70,6 +71,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new DefinePlugin({
+      rcms_js_config: JSON.stringify(config),
+    }),
     new VueLoaderPlugin(),
     new CleanWebpackPlugin(),
     new ManifestPlugin({ publicPath: '' }),
@@ -111,6 +115,7 @@ module.exports = {
     host: config.devHost,
     port: config.devPort,
     publicPath: config.publicPath,
+    disableHostCheck: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
