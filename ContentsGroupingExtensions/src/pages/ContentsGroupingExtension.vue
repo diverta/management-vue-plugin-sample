ce1cf33d58384aeb93b0a989ccb166a5992a5352
YuseiAbe <yabe@diverta.co.jp>
2022-06-30 14:51:06 +0900

    feat: add base component

---
+++ ContentsGroupingExtensions/src/pages/ContentsGroupingExtension.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        TEST
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+window.rcmsJS.vue.registerVM(Vue, rcms_js_config.publicPath); // eslint-disable-line
+
+export default {
+    props: ['extConfig'],
+};
+</script>
