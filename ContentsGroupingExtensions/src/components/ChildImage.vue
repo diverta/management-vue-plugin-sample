ce5c1c4a541961f1326b63bc569514781ca3ac02
YuseiAbe <yabe@diverta.co.jp>
2022-07-06 15:49:48 +0900

    feat(ChildImage): apply image upload

--- ContentsGroupingExtensions/src/components/ChildImage.vue
+++ ContentsGroupingExtensions/src/components/ChildImage.vue
@@ -7,55 +7,109 @@
             :data-ext_type="$attrs.ext_type"
             :data-default_value="$attrs.default_value"
         >
-            <p></p>
             <div v-show="!removes">
-                <div :id="`filesUploaded_preview${$attrs.ext_col_nm}-0`"></div>
+                <div v-if="fileURL">
+                    <a :href="fileURL" target="_blank">
+                        <img :src="fileURL" style="max-width:200px;" alt="" />
+                    </a>
+                    <input type="hidden" :name="`tmpfile_${$attrs.name}`" :value="fileURL" />
+                </div>
                 <br />
-                <div :id="`fileQueue${$attrs.ext_col_nm}-0`"></div>
-                <input type="file" name="Filedata" id="uploadify${$attrs.ext_col_nm}-0" v-show="false" />
+
+                <!-- <div :id="`fileQueue${$attrs.ext_col_nm}-0`"></div> -->
+
                 <div
-                    :id="`uploadifive-uploadifive${$attrs.ext_col_nm}-0`"
                     class="uploadifive-button"
                     style="height: 30px; line-height: 30px; overflow: hidden; position: relative; text-align: center; width: auto;"
                 >
-                    <i class="fa fa-plus"></i>
-                    画像を選択する
-                    <input type="file" name="Filedata" :id="`uploadifive${$attrs.ext_col_nm}-0`" v-show="false" />
+                    <i :class="['fa', fileURL ? 'fa-refresh' : 'fa-plus']"></i>
+                    {{ fileURL ? '画像を差し替える' : '画像を選択する' }}
+
                     <input
                         type="file"
+                        name="Filedata"
                         style="font-size: 30px; opacity: 0; position: absolute; right: -3px; top: -3px; z-index: 999;"
-                        accept="image/jpeg,image/gif,image/png,image/svg+xml"
+                        :accept="accepts.join(',')"
+                        @input="upload"
                     />
                 </div>
-                <div :id="`filesUploaded_msg${$attrs.ext_col_nm}-0`" />
+                <div v-if="msg">
+                    <span style="color:red;">{{ msg }}</span>
+                </div>
             </div>
             <div>
-                <input :name="`del_file_${$attrs.ext_col_nm}-0`" type="checkbox" value="1" @change="onChange" />
+                <input :name="`del_file_${$attrs.ext_col_nm}-0`" type="checkbox" value="1" @change="onApplyRemoves" />
                 削除する
             </div>
             <p></p>
-            <div v-show="!removes">
+            <div>
                 画像の説明:
-                <textarea :name="$attrs.name" rows="2" cols="60" />
+                <textarea :name="$attrs.name" rows="2" cols="60" v-model="description" />
             </div>
         </dd>
     </div>
 </template>
 
 <script>
-// TODO apply dynamic attrs
-// TODO file ext apply
-// TODO check & improve S3 ajax
 export default {
     data() {
         return {
             removes: false,
+            fileURL: null,
+            msg: null,
+            accepts: ['image/jpeg', 'image/gif', 'image/png', 'image/svg+xml'],
+            description: '',
         };
     },
     methods: {
-        onChange() {
+        onApplyRemoves() {
             this.removes = !this.removes;
         },
+        async requestUploadFile(file) {
+            const topicsGroupID = new URL(window.location.href).searchParams.get('topics_id');
+            const url = `/direct/topics/file_upload/?type=file%26width=%26height=%26topics_group_id=${topicsGroupID}%26ext_no=${this.$attrs.no}`;
+
+            const data = new FormData();
+            data.append('fileext', file.name.split('.').pop());
+            data.append('key', file.name);
+            data.append('Filedata', file);
+
+            const res = await fetch(url, {
+                method: 'POST',
+                body: data,
+                mode: 'cors',
+                credentials: 'include',
+            });
+            return res.text();
+        },
+        async upload($e) {
+            this.msg = null;
+
+            const file = $e.target.files[0];
+            const fileMB = file.size / 1024 / 1024;
+            if (fileMB > 10) {
+                this.msg = 'ファイルサイズが大きすぎます。';
+                return;
+            }
+
+            try {
+                const domStr = await this.requestUploadFile(file);
+                const template = document.createElement('template');
+                template.innerHTML = domStr;
+                this.fileURL = template.content.firstChild.getAttribute('href');
+                this.msg = '更新する準備ができました。';
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.error(e);
+                this.msg = e;
+            }
+        },
+    },
+    created() {
+        if (this.$attrs.file_url) {
+            this.fileURL = this.$attrs.file_url;
+        }
+        this.description = this.$attrs.value;
     },
 };
 </script>
