b3289e84912b7f840241a0ae41f2917f4bec106b
Nur Ahmad Wahid <nwahid@diverta.co.jp>
2023-08-24 20:49:54 +0900

    Disable relation type once

--- packages/ContentsGroupingExtensions/src/pages/ContentsGroupingExtension.vue
+++ packages/ContentsGroupingExtensions/src/pages/ContentsGroupingExtension.vue
@@ -152,7 +152,7 @@ export default {
 
         try {
             await this.loadScript(prefixUrl + manifest['rcms-mng-vendors.js']);
-            const coreComponents = ['Text', 'Textarea', 'Link', 'RelationFld'];
+            const coreComponents = ['Text', 'Textarea', 'Link'];
             await Promise.all(
                 coreComponents.map((component) =>
                     this.loadScript(prefixUrl + manifest['common/components/extensions/Ext' + component + '.js'])
