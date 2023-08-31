412f7e16089bdaf043257f7b1b9a12dbce8aaf8f
Nur Ahmad Wahid <nwahid@diverta.co.jp>
2023-08-31 23:51:39 +0900

    Replace custom plugin of relation with core

--- packages/ContentsGroupingExtensions/src/components/ChildRelationField/index.vue
+++ packages/ContentsGroupingExtensions/src/components/ChildRelationField/index.vue
@@ -10,13 +10,20 @@
             <dl>
                 <ExtRelationFld
                     v-bind="{
-                        ...$attrs,
-                        opts: $attrs.options,
+                        call_from_module: 'topics',
+                        setting_id: $attrs.topics_group_id,
+                        ext_no: $attrs.name.replace(/\[(\d+)\]/i, ''),
+                        selected_item: $attrs.value,
+                        table_id: $attrs.options.module,
+                        allowed_id: $attrs.options.group_id,
+                        limit: $attrs.options.limit,
+                        _name: $attrs.name,
+                        disp_group_no: $attrs.disp_group_no,
+                        ext_options: $attrs.options,
+                        ext_help_msg: $attrs.ext_help_msg,
                     }"
+                    v-if="flagToLoadOnce"
                 />
-                <p v-if="$attrs.ext_help_msg">
-                    <span class="hint">{{ $attrs.ext_help_msg }}</span>
-                </p>
             </dl>
         </dd>
     </div>
@@ -24,9 +31,27 @@
 
 <script>
 export default {
-    inheritAttrs: false,
-    components: {
-        ExtRelationFld: () => import('./ExtRelationFld/index.vue'),
+    props: {
+        isLoaded: { type: Boolean, required: true },
+    },
+    data() {
+        return {
+            flagToLoadOnce: false,
+        };
+    },
+    mounted() {
+        console.log(this.$attrs);
+    },
+    watch: {
+        isLoaded: {
+            immediate: true,
+            handler(newVal) {
+                if (newVal && !this.flagToLoadOnce) {
+                    this.$options.components.ExtRelationFld = window['common/components/extensions/ExtRelationFld'];
+                    this.flagToLoadOnce = true;
+                }
+            },
+        },
     },
 };
 </script>
