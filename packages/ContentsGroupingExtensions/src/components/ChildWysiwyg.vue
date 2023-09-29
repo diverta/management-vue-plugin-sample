6eca5c82e1c2143bd204b078612d2955c69312f6
Nur Ahmad Wahid <nwahid@diverta.co.jp>
2023-09-29 17:41:05 +0900

    Revert

--- packages/ContentsGroupingExtensions/src/components/ChildWysiwyg.vue
+++ packages/ContentsGroupingExtensions/src/components/ChildWysiwyg.vue
@@ -7,23 +7,20 @@
             :data-ext_type="$attrs.ext_type"
             :data-default_value="$attrs.default_value"
         >
-            <p v-if="!editorLoaded">読み込み中...</p>
-            <div :id="'editor-wrapper-' + id">
-                <ExtWysiwyg
-                    v-if="flagToLoadOnce"
-                    :name="$attrs.name"
-                    :value="$attrs.value"
-                    :opts="$attrs.options"
-                    :upload_dir="$attrs.upload_dir"
-                    :placeholders="$attrs.options.placeholders"
-                    :use_font_size_px="$attrs.options.use_font_size_px ? $attrs.options.use_font_size_px : 0"
-                    :resource_path="$attrs.options.resource"
-                    :ext_help_msg="$attrs.ext_help_msg"
-                    :delay="0"
-                    ckfinder="/management/wysiwyg"
-                    script_path="/management/wysiwyg/ckfinder/3.5.1/ckfinder.js"
-                />
-            </div>
+            <ExtWysiwyg
+                v-if="flagToLoadOnce"
+                :name="$attrs.name"
+                :value="$attrs.value"
+                :opts="$attrs.options"
+                :upload_dir="$attrs.upload_dir"
+                :placeholders="$attrs.options.placeholders"
+                :use_font_size_px="$attrs.options.use_font_size_px ? $attrs.options.use_font_size_px : 0"
+                :resource_path="$attrs.options.resource"
+                :ext_help_msg="$attrs.ext_help_msg"
+                :delay="0"
+                ckfinder="/management/wysiwyg"
+                script_path="/management/wysiwyg/ckfinder/3.5.1/ckfinder.js"
+            />
         </dd>
     </div>
 </template>
@@ -36,16 +33,7 @@ export default {
     data() {
         return {
             flagToLoadOnce: false,
-            editorLoaded: false,
         };
-    },
-    computed: {
-        id() {
-            return this.$attrs.name.replace(/\[(\d+)\]/i, '');
-        },
-    },
-    updated() {
-        this.checkWrapperContent();
     },
     watch: {
         isLoaded: {
@@ -56,13 +44,6 @@ export default {
                     this.flagToLoadOnce = true;
                 }
             },
-        },
-    },
-    methods: {
-        checkWrapperContent() {
-            const wrapper = this.$el.querySelector(`#editor-wrapper-${this.id}`);
-            const ckEditorDiv = wrapper.querySelector('.ck-editor'); // When CKEditor is loaded entirely, it will create a div with class .ck-editor
-            this.editorLoaded = !!ckEditorDiv;
         },
     },
 };
