8de0d8f7796931aa2dde38ab2364174364cd0d07
SHIBAZAKI Takeharu <shibazaki@diverta.co.jp>
2024-12-04 18:46:55 +0900

    feat: front_kuroco_document_site/issues/1202

---
+++ VueSample/src/pages/VueSample.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Vue Sample</h1>
+        <ul>
+            <li v-for="item in items" :key="item.topics_id">
+                {{ item.subject }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import Vue from 'vue';
+window.rcmsJS.vue.registerVM(Vue, rcms_js_config.publicPath); // eslint-disable-line
+const axios = require('axios').default;
+
+export default {
+    components: {},
+    props: {
+        root_api_url: {
+            type: String,
+            default: '',
+        },
+        endpoint: {
+            type: String,
+            default: '',
+        },
+    },
+    created: function() {},
+    mounted: async function() {
+        const resp = await axios.get(this.root_api_url + this.endpoint);
+        this.items = resp.data.list ? resp.data.list : [];
+    },
+    data() {
+        return {
+            items: [],
+        };
+    },
+
+    computed: {},
+};
+</script>
